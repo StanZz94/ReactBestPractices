1050460cdc83ed19dfb295d5eb8fb4f2bbf83a45
StanZz94 <123236911+StanZz94@users.noreply.github.com>
2025-07-09 04:05:16 +0300

    optimize open/close functions with toggle one

--- src/components/Accordion/AccordionItem.jsx
+++ src/components/Accordion/AccordionItem.jsx
@@ -2,15 +2,11 @@ import { useAccordionContext } from "./Accordion"
 
 export default function AccordionItem({ id, children, className, title }) {
 
-    const {openItemId, openItem, closeItem} = useAccordionContext();
+    const {openItemId, toggleItem} = useAccordionContext();
     const isOpen = openItemId === id;
 
     function handleClick() {
-        if (isOpen) {
-            closeItem()
-        } else {
-            openItem(id)
-        }
+        toggleItem(id);
     }
     return (
         <li className={className}>
