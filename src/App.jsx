d1bc1c229887a477115fbf513b82766816f3c1b4
StanZz94 <123236911+StanZz94@users.noreply.github.com>
2025-07-09 03:29:30 +0300

    create accordionItem component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,29 @@
+import Accordion from "./components/Accordion/Accordion";
+import AccordionItem from "./components/Accordion/AccordionItem";
+
 function App() {
-  return <h1>React Patterns & Practices</h1>;
+  return <main>
+    <section>
+      <h2>Why work with us?</h2>
+
+      <Accordion className="accordion">
+        <AccordionItem title="We got 20 years expirience!">
+          <article>
+            <p>Cant go wrong with us!</p>
+            <p>Bla bla bla blablalbal balbalbal ballblbalba</p>
+          </article>
+        </AccordionItem>
+
+        <AccordionItem title="We got 20 years expirience!">
+          <article>
+            <p>Cant go wrong with us!</p>
+            <p>Bla bla bla blablalbal balbalbal ballblbalba</p>
+          </article>
+        </AccordionItem>
+      </Accordion>
+    </section>
+  </main>
+
 }
 
 export default App;
