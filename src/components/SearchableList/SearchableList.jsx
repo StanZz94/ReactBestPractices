a6d1415d09a6ac695d237091470457019bbdbf9e
StanZz94 <123236911+StanZz94@users.noreply.github.com>
2025-07-15 23:52:05 +0300

    add PLACES data and rander list of items

--- src/components/SearchableList/SearchableList.jsx
+++ src/components/SearchableList/SearchableList.jsx
@@ -3,7 +3,7 @@ export default function SearchableList({ items }) {
     return <div className="searchable-list">
         <input type="search" placeholder="Search..." />
         <ul>
-            {items.map(item => (<li key={item}>{item}</li>))}
+            {items.map(item => (<li key={item.id}>{item.title}</li>))}
         </ul>
     </div>
 }
