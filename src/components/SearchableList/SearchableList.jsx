0c98b6941f9e28e8044a22b3eea23aa8f5c934e8
StanZz94 <123236911+StanZz94@users.noreply.github.com>
2025-07-15 23:46:59 +0300

    create SearchableItem component

---
+++ src/components/SearchableList/SearchableList.jsx
@@ -0,0 +1,9 @@
+export default function SearchableList({ items }) {
+
+    return <div className="searchable-list">
+        <input type="search" placeholder="Search..." />
+        <ul>
+            {items.map(item => (<li key={item}>{item}</li>))}
+        </ul>
+    </div>
+}
