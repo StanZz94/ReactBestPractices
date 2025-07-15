6ecf50b0f3aab80cf1de51359de088d19a4d0d20
StanZz94 <123236911+StanZz94@users.noreply.github.com>
2025-07-16 00:30:30 +0300

    use render props

--- src/components/SearchableList/SearchableList.jsx
+++ src/components/SearchableList/SearchableList.jsx
@@ -1,9 +1,21 @@
-export default function SearchableList({ items }) {
+import { useState } from "react"
+
+export default function SearchableList({ items, children }) {
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const searchResult = items.filter(item => JSON.stringify(item).toLowerCase().includes(searchTerm.toLowerCase()))
+
+    function handleChange(e) {
+        const searchTerm = e.target.value;
+        setSearchTerm(searchTerm);
+    }
 
     return <div className="searchable-list">
-        <input type="search" placeholder="Search..." />
+        <input type="search" onChange={handleChange} placeholder="Search..." />
         <ul>
-            {items.map(item => (<li key={item.id}>{item.title}</li>))}
+            {searchResult.map((item, index) => (<li key={index}>
+                {children(item)}
+                </li>))}
         </ul>
     </div>
 }
